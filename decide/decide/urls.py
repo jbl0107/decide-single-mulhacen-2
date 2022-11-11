381cf2350af038a55354519b328cc75e47359c9c
jbl0107 <ballestero20017@gmail.com>
2022-11-11 17:39:18 +0100

    #1 Configuración de traducciones

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -22,6 +22,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
