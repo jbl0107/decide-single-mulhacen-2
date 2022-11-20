f924d082d4b7f9d8a1853ee24ed466fee6a06df4
abernalmar <abernalmar@gmail.com>
2022-11-20 20:44:17 +0100

    #17 Página sobre EGC

--- decide/nuevaInfo/urls.py
+++ decide/nuevaInfo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import infoDecide
+from .views import infoEgc
 
 
 urlpatterns = [
     path('masInfo/', infoDecide)
+]
+
+urlpatterns = [
+    path('masInfoEgc/', infoEgc)
 ]
--- decide/nuevaInfo/views.py
+++ decide/nuevaInfo/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def infoDecide(request):
     return render(request, 'nuevaInfo/infoDecide.html')
+
+def infoEgc(request):
+    return render(request, 'nuevaInfo/infoEgc.html')
