bfab6b49ed6f62e8f7b7d7bbb8bb64bc2a1a3892
JuanAntonio4 <juanantoniomenavargas@gmail.com>
2022-12-08 23:34:18 +0100

    #87 Añadida gráfica de pastel a Bot de Telegram

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -47,11 +47,21 @@ class VisualizerView(TemplateView):
             plt.barh(opciones, votos)
             plt.savefig('barras_horizontales.png')
 
+            fig, ax = plt.subplots()
+            colores = ["#EE6055","#60D394","#AAF683"]
+            ax.pie(votos, labels=opciones, autopct="%0.1f %%" , colors=colores)
+            plt.axis("equal")
+            plt.savefig('pastel.png')
+
             with open('barras_horizontales.png', 'rb') as photo_file:
                             bot.sendPhoto(chat_id=chat_id,
                                 photo=photo_file,
-                                caption='Aqui esta una grafica de barras de la votacion')
+                                caption='Aqui tienes la gráfica de barras horizontales de su votación.')
 
+            with open('pastel.png', 'rb') as photo_file:
+                            bot.sendPhoto(chat_id=chat_id,
+                                photo=photo_file,
+                                caption='Aqui tienes la gráfica de pastel de su votación.')
     
         except:
             raise Http404
