bd0aa78cfde2495bf7e516d7e3a837978e840a0b
paomarsan <paomarsan@alum.us.es>
2022-12-17 18:01:29 +0100

    #130 Crear test de booth

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from base.tests import BaseTestCase 
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+
+from base import mods
+
+
+class BoothTestCase(BaseTestCase):
+
+    def test_booth_voting_id_negativo(self):
+        response = self.client.put('/booth/'+str(-1), follow=True)
+        self.assertEqual(response.status_code, 404)
