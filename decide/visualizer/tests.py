121bb6648f88f2f2ee7e64a1d0126fa06821bcd7
meriglmar <meriglmar@alum.us.es>
2022-12-17 19:30:50 +0100

    #95 Tests de visualizer implementados todos

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -76,4 +76,37 @@ class TranslationCase(StaticLiveServerTestCase):
         self.assertEqual(username_label.text, "Results:")
     
 
+    def test_spanish_translation(self):
+
+        self.driver.set_window_size(1920,1080)
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        self.driver.get("http://localhost:8080/visualizer/1/")
+        
+        language_selector = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.NAME, value="language"))
+        language_selector.click()
+        selected_language = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.CSS_SELECTOR, value="select > option:nth-child(2)"))
+        selected_language.click()
+        change_language_button = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.ID, value="boton-submit"))
+        change_language_button.click()
+        username_label = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.ID, value="confirm"))
+        self.assertEqual(username_label.text, "Resultados:")
+
+    def test_german_translation(self):
+
+        self.driver.set_window_size(1920,1080)
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        self.driver.get("http://localhost:8080/visualizer/1/")
+        
+        language_selector = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.NAME, value="language"))
+        language_selector.click()
+        selected_language = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.CSS_SELECTOR, value="select > option:nth-child(3)"))
+        selected_language.click()
+        change_language_button = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.ID, value="boton-submit"))
+        change_language_button.click()
+        username_label = WebDriverWait(self.driver, timeout=10).until(lambda d: d.find_element(by=By.ID, value="confirm"))
+        self.assertEqual(username_label.text, "Ergebnisse:")
 
